a2c4db10704af97d0099cee4292111d0f486c46b
Daniel Ferraz <dxferraz@gmail.com>
2025-02-24 16:05:22 -0300

    .

--- lib/features/historical_facts/presentation/widgets/historical_fact_section.dart
+++ lib/features/historical_facts/presentation/widgets/historical_fact_section.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
+import 'package:knowhen/i18n/app_localizations.dart';
 
 class HistoricalFactSection extends StatelessWidget {
   final String imageUrl;
